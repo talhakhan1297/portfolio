4a781ce67cd5adeeb285bad3dd44ecfcccd53bb2
Talha Javed Khan <talhakhan1297@yahoo.com>
2024-02-20 13:39:12 +0500

    Clear data

--- lib/style/colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-const kWhite = Color(0xFFFFFFFF);
-const kOrange = Color(0xFFF79C13);
-const kBlack = Color(0xFF0A0F13);
-const kDarkNavyBlue = Color(0xFF0D141A);
-const kNavyBlue = Color(0xFF111C26);
-const kGrey = Color(0xFF2C2C2C);
